e60eaebb8c338b77f093e14f219c1caf78b8c51c
Brian Rowe <briprowe@yahoo.com>
2013-01-04 22:08:01 -0500

    The deploy function works.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-s3-repo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Deploy third party artifacts to a maven repository hosted on Amazon S3."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :dependencies [[org.springframework.build/aws-maven "4.4.0.RELEASE"]])
--- src/leiningen/s3_repo.clj
+++ src/leiningen/s3_repo.clj
@@ -1,6 +1,30 @@
-(ns leiningen.s3-repo)
+(ns leiningen.s3-repo
+  (:require [leiningen.core.main  :as main]
+            [cemerick.pomegranate.aether :as aether])
+  (:import [org.springframework.build.aws.maven SimpleStorageServiceWagon]))
 
-(defn s3-repo
-  "I don't do a lot."
-  [project & args]
-  (println "Hi!"))
+(aether/register-wagon-factory! "s3" #(SimpleStorageServiceWagon.))
+
+(defn deploy
+  "Deploy a given artifact to S3."
+  [repo url username passphrase coords jar-file pom-file]
+  
+  (aether/deploy :repository {repo {:url url :username username :passphrase passphrase}}
+                 :coordinates coords
+                 :jar-file jar-file
+                 :pom-file pom-file))
+
+(defn print-help
+  []
+  (println "
+Leiningen plugin for managing a maven repository hosted on Amazon S3.
+
+deploy Deploy an artifact to S3.
+"))
+
+(defn ^:no-project-needed s3-repo
+  "Manage a maven repository hosted on Amazon S3."
+  [_ command & args]
+  (case command
+    "deploy" (apply deploy args)
+    (print-help)))
